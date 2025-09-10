b0b7cae802014123828467d4d2f78dc3babac20e
josejose <dylan.roblesf@konradlorenz.edu.co>
2025-09-10 15:49:26 -0500

    mensaje

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+
+    res.send('Hola mundo!')
+
+});
+
+app.listen(port, () => {
+
+    console.log('La aplicacion se esta ejecutando por el puerto' + port)
+
+
+});
